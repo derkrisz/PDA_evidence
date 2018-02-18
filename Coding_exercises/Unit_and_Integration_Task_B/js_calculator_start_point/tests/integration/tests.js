36dda995a796bd298da81c825dd4f3246cb3ee45
derkrisz <krisztian.der@gmail.com>
2018-02-18 11:31:14 +0000

    comment out old test for division by 0

--- Coding_exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
+++ Coding_exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/integration/tests.js
@@ -128,14 +128,14 @@ describe('calculator functionality', function() {
   })
 
   //original return value when divided by 0
-  it('should return an error when trying to divide by 0', function(){
-    running_total = element(by.css('#running_total'))
-    element(by.css('#number5')).click();
-    element(by.css('#operator_divide')).click();
-    element(by.css('#number0')).click();
-    element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('Infinity')
-  })
+  // it('should return an error when trying to divide by 0', function(){
+  //   running_total = element(by.css('#running_total'))
+  //   element(by.css('#number5')).click();
+  //   element(by.css('#operator_divide')).click();
+  //   element(by.css('#number0')).click();
+  //   element(by.css('#operator_equals')).click();
+  //   expect(running_total.getAttribute('value')).to.eventually.equal('Infinity')
+  // })
 
   it('should return an error when trying to divide by 0', function(){
     running_total = element(by.css('#running_total'))
